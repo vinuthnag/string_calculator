9a2c2f8c1776b36fed080b23239ac83668ac2cc2
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-08-05 21:54:14 +0530

    supports different delimeters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,4 +21,8 @@ RSpec.describe StringCalculator do
   it 'handles newline as a delimiter' do
     expect(StringCalculator.new.add("1\n2,3")).to eq(6)
   end
+
+  it 'supports different delimeters' do
+    expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+  end
 end
