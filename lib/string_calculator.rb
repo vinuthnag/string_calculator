e2f4cebbdfeb0b42b0b94708b279ccb66ab2351b
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-08-05 18:51:41 +0530

    when 2 numbers are passed

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    numbers.split(',').map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe StringCalculator do
   it 'returns thenumbers when it has one input' do
     expect(StringCalculator.new.add("5")).to eq(5)
   end
+
+  it 'returns sum of the two comma separated numbers' do
+    expect(StringCalculator.new.add("1,2")).to eq(3)
+  end
 end
